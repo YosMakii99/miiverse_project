a3dcebbd4961ae41fad0c165c6aef494c54870d7
amkr01 <yosmakiilosdinos@gmail.com>
2025-04-20 07:09:19 -0600

    Add files via upload

---
+++ server/miiverse_forwarder.c
@@ -0,0 +1,9 @@
+
+#include <wiiu/os.h>
+#include <wiiu/coreinit.h>
+#include <wiiu/sysapp.h>
+
+int main(int argc, char **argv) {
+    SYSLaunchBrowser("https://yosmakii99.github.io/project_miiverse/", 0);
+    return 0;
+}
